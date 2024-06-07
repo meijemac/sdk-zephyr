8c556d86df067e6707e53a6acaeb188d3e3f7cff
Maciej Meijer <maciej.meijer@nordicsemi.no>
2024-06-07 16:26:28 +0200

    NRFX-None: Change sample for soft off testing - for both app and rad

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -4,11 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/kernel.h>
 #include <stdio.h>
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	while(1)	{
+		k_sleep(K_FOREVER);
+	}
 
 	return 0;
 }
